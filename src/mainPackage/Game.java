a3c673d35bac126ac27d858b53690104142a2fd9
Schweem <63567335+Schweem@users.noreply.github.com>
2023-05-08 11:59:20 -0400

    Some structure that might suck

--- src/mainPackage/Game.java
+++ src/mainPackage/Game.java
@@ -12,13 +12,28 @@ public class Game {
     }
 
     public static void main(String[] args) {
+    	//initialize objects
     	GUI g = new GUI();
     	Player p = new Player();
     	Resource berries = new Resource(10, 10, "berries");
     	Resource sticks = new Resource(10, 10, "sticks");
     	Farm f = new Farm("Berry Farm", berries);
 
+    	//GUI Controllers (button conditions)
     	g.updateLabels(berries.getAmount(), berries.getMaximum(), sticks.getAmount(), sticks.getMaximum(), p.getHunger(), f.getCount());
+    	if (berries.getAmount() >= 100) {
+    		g.setMakeFarmButtonVisible(true);
+    	} else {
+    		g.setMakeFarmButtonVisible(false);
+    	}
+    	if (sticks.getAmount() >= 10) {
+    		g.setBasketButtonVisible(true);
+    	} else {
+    		g.setBasketButtonVisible(false);
+    	}
+    	
+    	//Game Loop
+    	
     	
     }
 
